edf9c8e0d3efbbec0a71c3f2ad58acd0928b6536
Pranjal-Pandey245 <81172387+Pranjal-Pandey245@users.noreply.github.com>
2022-01-20 12:46:34 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -2,20 +2,21 @@ function preload(){
 
 }
 
+function draw(){
+    image(video, 0, 0, 300, 300);
+
+}
+
 function setup(){
     canvas= createCanvas(300,300);
     canvas.center();
 
-    video= createCapture(Video);
+    video= createCapture(VIDEO);
     video.size(300, 300);
     video.hide()
 
    posenet= ml5.poseNet(video, modelLoaded);
    poseNet.on('pose', gotPoses);
-}
-
-function draw(){
-    image(video, 0, 0, 300, 300);
 }
 
 function modelLoaded(){
